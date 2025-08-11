178348e94a88ef1db0ce9d824cc5f5c31ea86c41
Sam Smith <stsmith313@gmail.com>
2025-08-11 09:40:51 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "1048317510761738240"; // your league ID
+export const leagueID = "1181415693643399168"; // your league ID
 export const leagueName = "Dynasty"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
